1e518e792566d605ccdd31b7791bbc9a50d7dde6
Anurag Sharma <96693300+CrossStack-Q@users.noreply.github.com>
2022-12-07 20:31:19 +0530

    Update

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 function Contact({}: Props) {
   return (
-    <div  className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <div  className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-sm lg:max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Contact
         </h3>
